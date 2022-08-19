c7bf92ff48fdc56d95bdd7eb020fc277a057608e
taehunlim <dsa1520@naver.com>
2022-08-19 17:11:54 +0900

    project init

---
+++ server.ts
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+import * as express from 'express';
+
+const app = express();
+
+const morgan = require('morgan');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const errorHandler = require('./_middleware/error-handler');
+
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+if (process.env.NODE_ENV === 'development') {
+   app.use(
+      cors({
+         origin: process.env.CLIENT_URL_TEST,
+      }),
+   );
+   app.use(morgan('dev'));
+} else {
+   app.use(
+      cors({
+         origin: process.env.CLIENT_URL,
+      }),
+   );
+}
+
+// global error
+app.use(errorHandler);
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
